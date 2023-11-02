75328f4a94d8cfe8b542da0014576457bb420806
Mathias <my-email@gmail.com>
2023-11-02 20:07:56 +0100

    Add demo project

---
+++ src/main/java/de/hacktalk/github/actions/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.hacktalk.github.actions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
